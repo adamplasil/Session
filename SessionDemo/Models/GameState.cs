3be959390dc804c9e4a049d967544abe9d5ac0ab
Adam Plašil <u17.421@pslib.cloud>
2019-12-09 13:25:14 +0100

    commit

---
+++ SessionDemo/Models/GameState.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SessionDemo.Models
+{
+    public class GameState
+    {
+        public string Player1Name { get; set; }
+        public string Player2Name { get; set; }
+
+    }
+}
---
+++ SessionDemo/Services/SessionStorage.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using SessionDemo.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace SessionDemo.Services
+{
+    public class SessionStorage<T> where T:new()
+    {
+        private ISession _session;
+        public SessionStorage(IHttpContextAccessor hca)
+        {
+            _session = hca.HttpContext.Session;
+        }
+        public T LoadOrCreate(string key)
+        {
+            T result = _session.Get<T>(key);
+            if (result == null)
+            {
+                result = new T();
+            }
+            return result;
+                
+        }
+        public void Save(string key, T data)
+        {
+            _session.Set(key, data);
+        }
+    }
+}
